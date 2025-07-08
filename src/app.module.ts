8ab6dc830e8191eeb1bd34d7d28dfdd60934e7d1
azmain-siam <siamhossain2700@gmail.com>
2025-07-09 05:35:08 +0600

    add: auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './main/user/user.module';
-import { PrismaServiceModule } from './main/prisma-service/prisma-service.module';
+import { PrismaModule } from './main/prisma-service/prisma-service.module';
 import { AuthModule } from './main/auth/auth.module';
 
 @Module({
-  imports: [UserModule, PrismaServiceModule, AuthModule],
+  imports: [UserModule, PrismaModule, AuthModule],
 })
 export class AppModule {}
