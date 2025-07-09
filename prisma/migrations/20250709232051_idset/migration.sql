dc8cb414d543b9f719963cd301c55250b63f9e3e
Mahammad Shariar Alam <mahammadshariaralam@gmail.com>
2025-07-10 05:38:03 +0600

    subscription table create successfully

--- prisma/migrations/20250709232051_idset/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "SubscriptionPlan" ADD CONSTRAINT "SubscriptionPlan_pkey" PRIMARY KEY ("id");
-
--- DropIndex
-DROP INDEX "SubscriptionPlan_id_key";
--- src/main/subscriptionplan/subscriptionplan.service.ts
+++ src/main/subscriptionplan/subscriptionplan.service.ts
@@ -7,17 +7,15 @@ export class SubscriptionplanService {
   constructor(private prisma: PrismaService) {}
   async create(createSubscriptionplanDto: CreateSubscriptionPlanDto) {
     try {
-      // const result = await this.prisma.subscriptionPlan.upsert({
-      //   where: { type: createSubscriptionplanDto.type },
-      //   update: {
-      //     length: createSubscriptionplanDto.length,
-      //     price: createSubscriptionplanDto.price,
-      //   },
-      //   create: {
-      //     ...createSubscriptionplanDto,
-      //   },
-      // });
-      // return result;
+      const result = await this.prisma.subscriptionPlan.upsert({
+        where: { type: createSubscriptionplanDto.type },
+        update: {},
+        create: {
+          ...createSubscriptionplanDto,
+        },
+      });
+
+      return result;
     } catch (error) {
       console.error('Error creating/upserting SubscriptionPlan:', error);
       throw new InternalServerErrorException(
