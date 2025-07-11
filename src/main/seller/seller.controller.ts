c59b29a1f9a0068fccbdb8b1e7d2d3777cbd012e
Mahammad Shariar Alam <mahammadshariaralam@gmail.com>
2025-07-12 00:39:06 +0600

    login and register done

--- src/main/seller/seller.controller.ts
+++ src/main/seller/seller.controller.ts
@@ -6,20 +6,23 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
+  Req,
 } from '@nestjs/common';
 import { SellerService } from './seller.service';
 import { CreateSellerDto } from './dto/create-seller.dto';
 import { UpdateSellerDto } from './dto/update-seller.dto';
+import { AuthGuard } from 'src/guards/jwt-auth.guard';
+import { Request } from 'express';
 
+@UseGuards(AuthGuard)
 @Controller('seller')
 export class SellerController {
   constructor(private readonly sellerService: SellerService) {}
 
   @Post()
-  create(@Body() createSellerDto: CreateSellerDto) {
-    const userId = '5c5e6159-3c0b-4596-bda9-f87726f28a32';
-
-    return this.sellerService.create(createSellerDto, userId);
+  create(@Body() createSellerDto: CreateSellerDto, @Req() req: Request) {
+    return this.sellerService.create(createSellerDto, req['userid'] as string);
   }
 
   @Get()
